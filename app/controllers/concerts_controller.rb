e59723a85451a96ef661d21eff1e7702bc3882da
Tracy Albernaz <tracyalbernaz@gmail.com>
2022-01-12 17:17:02 -0500

    created concert show page

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -6,27 +6,5 @@ class ConcertsController < ApplicationController
 
   def show 
     @concert = Concert.find(params[:id])
-    # @reviews = @restaurant.reviews
-    # @catergories = restaurant.catergories
   end
-
-  # def new 
-  #   @restaurant = Restaurant.new
-  # end
-
-  # def create 
-  #   def restaurant_params
-  #     params.require(:restaurant).permit(:name, :address, :city, :state, :zip, :description)
-  #   end
-   
-  #   @restaurant = Restaurant.new(restaurant_params)
-
-  #   if @restaurant.save
-  #     flash[:notice] = "Restaurant added successfully"
-  #     redirect_to @restaurant
-  #   else
-  #     flash.now[:error] = @restaurant.errors.full_messages.to_sentence
-  #     render :new
-  #   end
-  # end
 end
